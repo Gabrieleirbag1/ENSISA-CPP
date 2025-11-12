e611b67ccf221713b6e337aa106c701d83f313a5
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-12 10:56:56 +0100

    refactor: improve search method implementation and formatting in SearchingAlgo class

--- TD2/Exo1/searching_algo.cpp
+++ TD2/Exo1/searching_algo.cpp
@@ -1,15 +1,21 @@
 
-#include<iostream>
+#include <iostream>
 #include "searching_algo.h"
 
 SearchingAlgo::SearchingAlgo() {}
 
-int SearchingAlgo::search(std::vector<int> elements, int searchKey) {
-    for (int num : elements) {
-        std::cout << num << std::endl;
+int SearchingAlgo::search(std::vector<int> elements, int searchKey)
+{
+    unsigned int vecSize = elements.size();
+    for (unsigned int i = 0; i < vecSize; i++)
+    {
+        if (elements[i] == searchKey) {
+            return i;
+        }
     }
+    return -1;
 }
 
-void SearchingAlgo::displaySearchResults(std::ostream& os, int result, int target) {
-
+void SearchingAlgo::displaySearchResults(std::ostream &os, int result, int target)
+{
 }
