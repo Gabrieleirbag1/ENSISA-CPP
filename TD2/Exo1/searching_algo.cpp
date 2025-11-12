a369959a5ee85d05982acd014efcd06bb6c79ebd
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-12 10:52:05 +0100

    feat: implement search method in SearchingAlgo class

--- TD2/Exo1/searching_algo.cpp
+++ TD2/Exo1/searching_algo.cpp
@@ -4,6 +4,12 @@
 
 SearchingAlgo::SearchingAlgo() {}
 
+int SearchingAlgo::search(std::vector<int> elements, int searchKey) {
+    for (int num : elements) {
+        std::cout << num << std::endl;
+    }
+}
+
 void SearchingAlgo::displaySearchResults(std::ostream& os, int result, int target) {
 
 }
