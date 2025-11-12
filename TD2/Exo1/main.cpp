0cb23e714733cfcea3375b6c4f3e9d16838850aa
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-12 13:18:29 +0100

    feat: testing

--- TD2/Exo1/main.cpp
+++ TD2/Exo1/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 int main() {
     LinearSearch linearObj;
-    vector<int> testVector = {1, 4, 2, 3, 5};
-    int results = linearObj.search(testVector, 3);
+    vector<int> testVector = {1, 0, 2, 3, 5};
+    vector<int> testVector2 = {1, 4, 2, 3, 5};
+
+    int results = linearObj.search(testVector, 4);
     linearObj.displaySearchResults(std::cout, results, 4);
+    std::cout << "----------------" << std::endl;
+    int results2 = linearObj.search(testVector2, 4);
+    linearObj.displaySearchResults(std::cout, results2, 4);
+
     return 0;
 }
