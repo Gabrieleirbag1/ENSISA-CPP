817b2859d8717ca17d3dceac3390205268d82f04
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-14 16:40:10 +0100

    feat: add var def

--- TD2/Exo2/matrix_base.cpp
+++ TD2/Exo2/matrix_base.cpp
@@ -7,3 +7,18 @@ template <class T>
 MatrixBase<T>::MatrixBase()
 {
 }
+
+template <class T>
+void MatrixBase<T>::addElement(int rowIndex, int colIndex)
+{}
+template <class T>
+vector<T, T> MatrixBase<T>::getElement(int rowIndex, int colIndex) const
+{}
+
+template <class T>
+size_t MatrixBase<T>::getRows() const
+{}
+
+template <class T>
+size_t MatrixBase<T>::getCols() const
+{}
