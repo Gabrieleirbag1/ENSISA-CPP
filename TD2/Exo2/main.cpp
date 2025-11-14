f97ce2ac65d3e555963f7f0eeaf1488596e3d62a
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-14 22:38:46 +0100

    testing

--- TD2/Exo2/main.cpp
+++ TD2/Exo2/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 int main() {
 
-    MatrixBase<int> matrixObj;    
+    MatrixBase<int> matrixObj;   
+    
+    MatrixBase<int> matrixObj2(5, 5, 0);
 
+    matrixObj2.addElement(0, 0, 5);
+
+    vector<vector<int>> test = matrixObj2.getElement(5);
+
+    matrixObj2.display(cout);
+    
     return 0;
 }
