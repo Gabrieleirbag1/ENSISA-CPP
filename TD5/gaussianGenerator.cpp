e38fa5ede876dc7f89a8c61f739a947cbc4dffa4
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-12 09:03:25 +0100

    feat: implement generateTimeseries method in GaussianGenerator class

--- TD5/gaussianGenerator.cpp
+++ TD5/gaussianGenerator.cpp
@@ -4,6 +4,21 @@ GaussianGenerator::GaussianGenerator() : TimeSeriesGenerator() {}
 
 GaussianGenerator::GaussianGenerator(int _seed) : TimeSeriesGenerator(_seed) {}
 
+vector<double> GaussianGenerator::generateTimeseries(int length) const {
+    vector<double> series;
+    series.reserve(length);
+
+    for (int i = 0; i < length; ++i) {
+        double u1 = static_cast<double>(rand()) / RAND_MAX;
+        double u2 = static_cast<double>(rand()) / RAND_MAX;
+        double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+        double value = z0 * ecartType + moyenne;
+        series.push_back(value);
+    }
+
+    return series;
+}
+
 double GaussianGenerator::getMoyenne() const {
     return moyenne;
 }
@@ -19,3 +34,4 @@ void GaussianGenerator::setMoyenne(double _moyenne) {
 void GaussianGenerator::setEcartType(double _ecartType) {
     ecartType = _ecartType;
 }
+
--- TD5/gaussianGenerator.h
+++ TD5/gaussianGenerator.h
@@ -2,12 +2,14 @@
 #define GAUSSIAN_GENERATOR_H
 
 #include "timeSeriesGenerator.h"
+#include <cmath>
 
 class GaussianGenerator : public TimeSeriesGenerator {
     public:
         GaussianGenerator();
         GaussianGenerator(int);
 
+        vector<double> generateTimeseries(int) const override;
 
         double getMoyenne() const;
         double getEcartType() const;
